e0fa9c86bf152a09d84c2d1661af1423e90cd261
boyoung jeon <boyoungjeon@boyoungs-MacBook-Pro.local>
2020-09-06 17:38:18 +0900

    비밀정보보호

---
+++ config/key.js
@@ -0,0 +1,6 @@
+if(process.env.MODE_ENV === 'production') {
+    module.exports = require('./prod');
+} else {
+
+    module.exports = require('./dev');
+}
--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+//user 모델을 가져온다.
+const { User } = require('./models/User');
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json
+app.use(bodyParser.json());
+
+
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://vboyoung:1234@boilerplate.ee8fh.mongodb.net/boilerplate?retryWrites=true&w=majority', {
-    useUnifiedTopology:true, 
-    useCreateIndex:true, 
-    useFindAndModify:false
-})
-.then(() => console.log('MongoDb Connected...'))
+mongoose.connect(config.mongoURI, {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDb Connected...'))
 .catch(err => console.log(err))
 
 
@@ -16,9 +26,24 @@ mongoose.connect('mongodb+srv://vboyoung:1234@boilerplate.ee8fh.mongodb.net/boil
 app.get('/', (req, res) => {
   res.send('Hello World! 안녕하세요')
 })
+ 
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.post('/register', (req, res) => {
+
+    //회원 가입 할때 필요한 정보들을 clien에서 가져오면, 그것들은 데이터 베이스에 넣어준다.
+
+    const user = new User(req.body)
+   
+    user.save((err, userInfo) => {
+        if(err) return res.json({ sucess: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+
 })
 
 
+const port = 5000
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
